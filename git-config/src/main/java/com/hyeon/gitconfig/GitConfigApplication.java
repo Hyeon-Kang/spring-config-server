991eb86e847451b81af30a0b4ef96230d437cbca
Hyeon-Kang <rkd099@naver.com>
2022-07-05 15:39:19 +0900

    @EnableConfigServer add

--- git-config/src/main/java/com/hyeon/gitconfig/GitConfigApplication.java
+++ git-config/src/main/java/com/hyeon/gitconfig/GitConfigApplication.java
@@ -2,8 +2,10 @@ package com.hyeon.gitconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class GitConfigApplication {
 
 	public static void main(String[] args) {

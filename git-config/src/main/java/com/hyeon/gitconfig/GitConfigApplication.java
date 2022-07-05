a46166b80fb84d8bc426912126d618c62f305a80
Hyeon-Kang <rkd099@naver.com>
2022-07-05 15:08:40 +0900

    spring init

---
+++ git-config/src/main/java/com/hyeon/gitconfig/GitConfigApplication.java
@@ -0,0 +1,13 @@
+package com.hyeon.gitconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitConfigApplication.class, args);
+	}
+
+}
